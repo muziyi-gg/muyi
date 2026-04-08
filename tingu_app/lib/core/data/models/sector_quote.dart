67db846fc0bf6ab8512fd6f6b2483eb4a6305f4e
muziyi-gg <59251813+muziyi-gg@users.noreply.github.com>
2026-04-08 21:57:09 +0800

    Add tingu_app/lib/core/data/models/sector_quote.dart

---
+++ tingu_app/lib/core/data/models/sector_quote.dart
@@ -0,0 +1,32 @@
+/// 板块行情数据模型
+class SectorQuote {
+  final String code;
+  final String name;
+  final double changePercent;
+  final double amount;
+  final int timestamp;
+
+  SectorQuote({
+    required this.code,
+    required this.name,
+    required this.changePercent,
+    required this.amount,
+    required this.timestamp,
+  });
+
+  factory SectorQuote.fromJson(Map<String, dynamic> json) => SectorQuote(
+    code: json['code'] ?? '',
+    name: json['name'] ?? '',
+    changePercent: (json['changePercent'] as num?)?.toDouble() ?? 0.0,
+    amount: (json['amount'] as num?)?.toDouble() ?? 0.0,
+    timestamp: json['timestamp'] ?? DateTime.now().millisecondsSinceEpoch,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'code': code,
+    'name': name,
+    'changePercent': changePercent,
+    'amount': amount,
+    'timestamp': timestamp,
+  };
+}
